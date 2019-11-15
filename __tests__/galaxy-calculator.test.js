d46ec065ae5e99dd0c0318404466d72506f5564b
Saul Ordonez <sauleordonez@gmail.com>
2019-11-15 14:02:14 -0800

    refactored years left tests

--- __tests__/galaxy-calculator.test.js
+++ __tests__/galaxy-calculator.test.js
@@ -6,6 +6,7 @@ describe('Earth Age', () => {
       let planetAge= new PlanetAge(28);
       expect(planetAge.earthAge).toEqual(28);
     });
+
 });
 
 describe('Planet Age', () => {
@@ -17,36 +18,17 @@ describe('Planet Age', () => {
       expect(planetAge.calculateMarsAge()).toEqual(14);
       expect(planetAge.calculateJupiterAge()).toEqual(2);
     });
+
 });
 
-describe('Mercury Years Left', () => {
+describe('Planet Years Left', () => {
 
-    test('should return users remaining years left in mercury years', () => {
-      let mercuryYearsLeft = new PlanetAge(28);
-      expect(mercuryYearsLeft.calculateYearsLeftMercury()).toEqual(184);
+    test('should return users remaining years left for specified planet', () => {
+      let planetYearsLeft = new PlanetAge(28);
+      expect(planetYearsLeft.calculateYearsLeftMercury()).toEqual(184);
+      expect(planetYearsLeft.calculateYearsLeftVenus()).toEqual(71);
+      expect(planetYearsLeft.calculateYearsLeftMars()).toEqual(24);
+      expect(planetYearsLeft.calculateYearsLeftJupiter()).toEqual(4);
     });
-});
 
-describe('Venus Years Left', () => {
-
-    test('should return users remaining years left in venus years', () => {
-      let venusYearsLeft = new PlanetAge(28);
-      expect(venusYearsLeft.calculateYearsLeftVenus()).toEqual(71);
-    });
-});
-
-describe('Mars Years Left', () => {
-
-    test('should return users remaining years left in mars years', () => {
-      let marsYearsLeft = new PlanetAge(28);
-      expect(marsYearsLeft.calculateYearsLeftMars()).toEqual(24);
-    });
-});
-
-describe('Jupiter Years Left', () => {
-
-    test('should return users remaining years left in jupiter years', () => {
-      let jupiterYearsLeft = new PlanetAge(28);
-      expect(jupiterYearsLeft.calculateYearsLeftJupiter()).toEqual(4);
-    });
 });
