5cec01c6ec400b0e1097508cf454a21cacbbd2e2
Saul Ordonez <sauleordonez@gmail.com>
2019-11-15 12:10:13 -0800

    refactor tests

--- __tests__/galaxy-calculator.test.js
+++ __tests__/galaxy-calculator.test.js
@@ -1,6 +1,6 @@
 import { PlanetAge } from './../src/backend.js'
 
-describe('Planet Age', () => {
+describe('Earth Age', () => {
 
     test('should record the age inputted from user', () => {
       let planetAge= new PlanetAge(28);
@@ -8,34 +8,37 @@ describe('Planet Age', () => {
     });
 });
 
-describe('Mercury Age', () => {
+describe('Planet Age', () => {
 
     test('should return users age in Mercury years', () => {
-      let mercuryAge = new PlanetAge(28);
-      expect(mercuryAge.calculateMercuryAge()).toEqual(116);
+      let planetAge = new PlanetAge(28);
+      expect(planetAge.calculateMercuryAge()).toEqual(116);
+      expect(planetAge.calculateVenusAge()).toEqual(45);
+      expect(planetAge.calculateMarsAge()).toEqual(14);
+      expect(planetAge.calculateJupiterAge()).toEqual(2);
     });
 });
 
-describe('Venus Age', () => {
-
-    test('should return users age in Venus years', () => {
-      let venusAge = new PlanetAge(28);
-      expect(venusAge.calculateVenusAge()).toEqual(45);
-    });
-});
-
-describe('Mars Age', () => {
-
-    test('should return users age in Mars years', () => {
-      let marsAge = new PlanetAge(28);
-      expect(marsAge.calculateMarsAge()).toEqual(14);
-    });
-});
-
-describe('Jupiter Age', () => {
-
-    test('should return users age in Jupiter years', () => {
-      let jupiterAge = new PlanetAge(28);
-      expect(jupiterAge.calculateJupiterAge()).toEqual(2);
-    });
-});
+// describe('Venus Age', () => {
+//
+//     test('should return users age in Venus years', () => {
+//       let venusAge = new PlanetAge(28);
+//
+//     });
+// });
+//
+// describe('Mars Age', () => {
+//
+//     test('should return users age in Mars years', () => {
+//       let marsAge = new PlanetAge(28);
+//
+//     });
+// });
+//
+// describe('Jupiter Age', () => {
+//
+//     test('should return users age in Jupiter years', () => {
+//       let jupiterAge = new PlanetAge(28);
+//
+//     });
+// });
