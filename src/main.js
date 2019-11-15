1a13750d472868f958877ed565f1ba67886a3a15
Saul Ordonez <sauleordonez@gmail.com>
2019-11-15 08:58:10 -0800

    adjust template values to correspond with current project

--- src/main.js
+++ src/main.js
@@ -1,9 +1,5 @@
-// import { test } from  './dice-game.js';
+// import { test } from  './backend.js';
 import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
-
-// function test2() {
-//   console.log('this is the front end');
-// } test2();
--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,7 @@ module.exports = {
   new UglifyJsPlugin({ sourceMap: true }),
   new CleanWebpackPlugin(),
   new HtmlWebpackPlugin({
-    title: 'Dice Game',
+    title: 'Galaxy Age Calculator',
     template: './src/index.html',
     inject: 'body'
   })
