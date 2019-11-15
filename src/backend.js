7da990938c8825e17c15b400127e7c5d525a0c5f
Saul Ordonez <sauleordonez@gmail.com>
2019-11-15 14:29:39 -0800

    add objects to Contructor

--- src/backend.js
+++ src/backend.js
@@ -1,11 +1,15 @@
 export class PlanetAge {
   constructor(earthAge, LifeExpentancyBirth) {
   this.earthAge = earthAge;
-  this.LifeExpentancyBirth = 72;
+  this.lifeExpentancyBirth = 72;
   this.mercuryAge = 0;
   this.venusAge = 0;
   this.marsAge = 0;
   this.jupiterAge = 0;
+  this.mercuryLEB = 0;
+  this.venusLEB = 0;
+  this.marsLEB = 0;
+  this.jupiterLEB = 0;
   }
 
   calculateMercuryAge() {
@@ -29,30 +33,36 @@ export class PlanetAge {
   }
 
   calculateYearsLeftMercury() {
-    let mercuryAge = parseInt(this.earthAge / 0.24);
-    let mercuryLEB = parseInt(this.LifeExpentancyBirth / 0.24);
-    let mercuryYearsLeft = mercuryLEB - mercuryAge;
+    this.mercuryAge = parseInt((this.earthAge / 0.24));
+    this.mercuryLEB = parseInt(this.lifeExpentancyBirth / 0.24);
+    let mercuryYearsLeft = this.mercuryLEB - this.mercuryAge;
     return mercuryYearsLeft
   }
 
   calculateYearsLeftVenus() {
-    let venusAge = parseInt(this.earthAge / 0.62);
-    let venusLEB = parseInt(this.LifeExpentancyBirth / 0.62);
-    let venusYearsLeft = venusLEB - venusAge;
+    this.venusAge = parseInt(this.earthAge / 0.62);
+    this.venusLEB = parseInt(this.lifeExpentancyBirth / 0.62);
+    let venusYearsLeft = this.venusLEB - this.venusAge;
     return venusYearsLeft
   }
 
   calculateYearsLeftMars() {
-    let marsAge = parseInt(this.earthAge / 1.88);
-    let marsLEB = parseInt(this.LifeExpentancyBirth / 1.88);
-    let marsYearsLeft = marsLEB - marsAge;
+    this.marsAge = parseInt(this.earthAge / 1.88);
+    this.marsLEB = parseInt(this.lifeExpentancyBirth / 1.88);
+    let marsYearsLeft = this.marsLEB - this.marsAge;
     return marsYearsLeft
   }
 
   calculateYearsLeftJupiter() {
-    let jupiterAge = parseInt(this.earthAge / 11.86);
-    let jupiterLEB = parseInt(this.LifeExpentancyBirth / 11.86);
-    let jupiterYearsLeft = jupiterLEB - jupiterAge;
+    this.jupiterAge = parseInt(this.earthAge / 11.86);
+    this.jupiterLEB = parseInt(this.lifeExpentancyBirth / 11.86);
+    let jupiterYearsLeft = this.jupiterLEB - this.jupiterAge;
     return jupiterYearsLeft
   }
+  //
+  // calculateMercuryOverLEB() {
+  //   if (this.earthAge > 72) {
+  //     let mercuryAgeOverLEB =
+  //   }
+  // }
 };
