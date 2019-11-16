1018ead43c0cdfaa8896c0f50954dfc8d46b20c8
Saul Ordonez <sauleordonez@gmail.com>
2019-11-15 16:51:29 -0800

    fix build errors

--- src/backend.js
+++ src/backend.js
@@ -1,15 +1,15 @@
 export class PlanetAge {
-  constructor(earthAge, lifeExpectancyBirth) {
-  this.earthAge = earthAge;
-  this.lifeExpectancyBirth = 72;
-  this.mercuryAge = 0;
-  this.venusAge = 0;
-  this.marsAge = 0;
-  this.jupiterAge = 0;
-  this.mercuryLEB = 0;
-  this.venusLEB = 0;
-  this.marsLEB = 0;
-  this.jupiterLEB = 0;
+  constructor(earthAge) {
+    this.earthAge = earthAge;
+    this.lifeExpectancyBirth = 72;
+    this.mercuryAge = 0;
+    this.venusAge = 0;
+    this.marsAge = 0;
+    this.jupiterAge = 0;
+    this.mercuryLEB = 0;
+    this.venusLEB = 0;
+    this.marsLEB = 0;
+    this.jupiterLEB = 0;
   }
 
   calculateMercuryAge() {
@@ -36,21 +36,21 @@ export class PlanetAge {
     this.mercuryAge = parseInt((this.earthAge / 0.24));
     this.mercuryLEB = parseInt(this.lifeExpectancyBirth / 0.24);
     let mercuryYearsLeft = this.mercuryLEB - this.mercuryAge;
-    return mercuryYearsLeft
+    return mercuryYearsLeft;
   }
 
   calculateYearsLeftVenus() {
     this.venusAge = parseInt(this.earthAge / 0.62);
     this.venusLEB = parseInt(this.lifeExpectancyBirth / 0.62);
     let venusYearsLeft = this.venusLEB - this.venusAge;
-    return venusYearsLeft
+    return venusYearsLeft;
   }
 
   calculateYearsLeftMars() {
     this.marsAge = parseInt(this.earthAge / 1.88);
     this.marsLEB = parseInt(this.lifeExpectancyBirth / 1.88);
     let marsYearsLeft = this.marsLEB - this.marsAge;
-    return marsYearsLeft
+    return marsYearsLeft;
   }
 
   calculateYearsLeftJupiter() {
@@ -58,7 +58,7 @@ export class PlanetAge {
 
     this.jupiterLEB = parseInt(this.lifeExpectancyBirth / 11.86);
     let jupiterYearsLeft = this.jupiterLEB - this.jupiterAge;
-    return jupiterYearsLeft
+    return jupiterYearsLeft;
   }
 
   calculateMercuryOverLEB() {
--- src/main.js
+++ src/main.js
@@ -8,6 +8,6 @@ $(document).ready(function() {
   $('form#inputForm').submit(function(event) {
     event.preventDefault();
 
-    
-  })
-})
+
+  });
+});
