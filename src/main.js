cd452356b9f91a2648479b3bd9010cf6d65e36e4
Saul Ordonez <sauleordonez@gmail.com>
2019-11-15 16:17:48 -0800

    add form to html

--- src/main.js
+++ src/main.js
@@ -1,5 +1,13 @@
-// import { test } from  './backend.js';
+import { PlanetAge } from  './backend.js';
 import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
+
+$(document).ready(function() {
+  $('form#inputForm').submit(function(event) {
+    event.preventDefault();
+
+    
+  })
+})
